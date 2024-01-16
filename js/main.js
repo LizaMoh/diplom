c3c7e145f10597726c14a65e18a73bc03af8418b
LizaMoh <143883888+LizaMoh@users.noreply.github.com>
2024-01-16 20:48:54 +0300

    уцка

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
 //slider
-const slides = document.querySelectorAll('.container'),
-prev = document.querySelector('.ec-button-prev'),
-next = document.querySelector('.tc-button-next');
-let slideIndex = 1;
+const slides1 = document.querySelectorAll('.container'),
+prev1 = document.querySelector('.ec-button-prev'),
+next1 = document.querySelector('.tc-button-next');
+let slideIndex1 = 1;
 
 function showSlides(n){
     if(n>slides.length){
-        slideIndex = 1;
+        slideIndex1 = 1;
     }
     if(n<1){
-        slideIndex = slides.length;
+        slideIndex1 = slides1.length;
     }
-    if(slides.length <10){
+    if(slides1.length <10){
         total.textContent = `0${slides.length}`;
     }else{
-        total.textContent = slides.length;
+        total.textContent = slides1.length;
     }
     slides.forEach(item=> item.style.display="none");
-    slides[slideIndex-1].style.display ="block";
+    slides[slideIndex1-1].style.display ="block";
 
     if(slides.length<10){
-        current.textContent = `0${slideIndex}`;
+        current.textContent = `0${slideIndex1}`;
     }else{
-        current.textContent = slideIndex;
+        current.textContent = slideIndex1;
     }
 }
 
